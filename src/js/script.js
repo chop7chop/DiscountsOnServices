b221121b1839d6bd0a440dadf6ea1ce418ba4214
Maksym <kondenkomaksim@gmail.com>
2022-08-05 20:47:14 +0300

    added tablet version

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 const mobBtn = document.querySelector('.menu__btn');
-const mobMenu = document.querySelector('.menu__wrapper');
+const mobMenu = document.querySelector('.menu__mob');
 
 mobBtn.addEventListener('click', function() {
-    mobMenu.classList.toggle('menu__wrapper_active');
+    mobMenu.classList.toggle('menu__mob_active');
     mobBtn.classList.toggle('menu__btn_active')
 });
